d0c4aea36b53a3b95f4a68cb561ee637eda8563b
Nick Pantev <103816194+IYI-Pantev@users.noreply.github.com>
2025-05-29 21:17:30 +0300

    next basic config

---
+++ app/login/page.tsx
@@ -0,0 +1,13 @@
+const Login = () => {
+  return (
+    <div className="flex flex-col items-center justify-center bg-niceBlue">
+      <h2 className="text-2xl font-bold text-white">Login Page</h2>
+      <p className="mt-2 text-white">
+        Please enter your credentials to log in.
+      </p>
+      {/* Add your login form here */}
+    </div>
+  );
+};
+
+export default Login;
